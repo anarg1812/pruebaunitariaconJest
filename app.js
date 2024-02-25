5ec8f6009876994f9ba72aaa64651a74743fc473
anarg1812 <147712294+anarg1812@users.noreply.github.com>
2024-02-25 19:04:53 +0000

    prueba unitaria con Jest

---
+++ app.js
@@ -0,0 +1,33 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+constfromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+module.exports = {oneEuroIs, fromEuroToDollar }
+letoneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+console.log(fromEuroToDollar(1));
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5 / 1.07;
+
+    return valueInYen;
+}
+console.log(fromDollarT(1))
---
+++ test.js
@@ -0,0 +1,24 @@
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
